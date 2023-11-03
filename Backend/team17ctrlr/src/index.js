087b4cb070fc1f2971e3b8cad9fecc4b8a020214
DEVSOG12 <oreofesolarin@gmail.com>
2023-11-03 17:42:17 -0400

    Fixed Boiler PLate

--- Backend/team17ctrlr/src/index.js
+++ Backend/team17ctrlr/src/index.js
@@ -1,7 +1,24 @@
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../.ctrl-r-team17-firebase-adminsdk-bnuam-5257d7ed3b.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const port = process.env.PORT || 2020;
+
+admin.app().firestore().collection('Users').get().then((snapshot) => {
+  snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+  });
+  return snapshot;
+}
+);
 app.listen(port, () => {
+  
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
