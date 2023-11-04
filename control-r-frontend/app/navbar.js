2a9af74be2196fdddd10132d3153d1d7b509f32f
T-Orisadare <pndebele@wesleyan.edu>
2023-11-04 00:14:15 -0400

    connecting comps

--- control-r-frontend/app/navbar.js
+++ control-r-frontend/app/navbar.js
@@ -1,39 +1,75 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import { authentication } from "@/firebaseConfig";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Avatar,
+  Button,
+} from "@nextui-org/react";
 
 export default function Nav() {
   return (
     <Navbar>
       <NavbarBrand>
-        <p className="font-bold text-inherit">CTRL-R</p>
+        <p className="font-bold text-inherit">
+          <Link color="foreground" href="/">
+            CTRL-R{" "}
+          </Link>
+        </p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
+        {authentication.currentUser ? (
+          <>
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Certifications
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive>
+              <Link href="./leaderboard" aria-current="page">
+                Leaderboard
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="./calendar">
+                Calendar
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Profile
+              </Link>
+            </NavbarItem>
+          </>
+        ) : (
+          <>
+            <NavbarItem isActive>
+              <Link href="./leaderboard" aria-current="page">
+                Leaderboard
+              </Link>
+            </NavbarItem>
+          </>
+        )}
       </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+      {authentication.currentUser ? (
+        <NavbarContent justify="end">
+            <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+        </NavbarContent>
+      ) : (
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            <Link href="/signIn">Login</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} color="primary" href="./signUp" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      )}
     </Navbar>
   );
 }
