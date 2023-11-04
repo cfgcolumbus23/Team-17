635069387ef275540c1ba0d8cac9678213cd27bb
Finn Bledsoe <bledsoef@berea.edu>
2023-11-04 05:50:38 -0400

    successfully rendering user information and information, need to show points still

--- control-r-frontend/app/admin/page.js
+++ control-r-frontend/app/admin/page.js
@@ -17,9 +17,9 @@ export default function App() {
     }
     var getUsers = async () => {
         // var data = await fetch(`http://ec2-3-82-130-200.compute-1.amazonaws.com:2020/api/v1/adminEndpoint`)
+        var data = await fetch(`http://localhost:2020/api/v1/adminReport/adminReports`)
         var jsonData = await data.json()
         setArray(jsonData)
-        console.log(array)
     }
 
     useEffect(() => {
@@ -30,33 +30,37 @@ export default function App() {
     return (
 
 
-        <div class="bg-gray h-screen">
-            <div class="bg-gray text-black p-15">
+        <div className="bg-gray h-screen">
+            <div className="bg-gray text-black p-15">
+                <p className="text-xl font-bold">Admin View</p>
+                <p className="text-base">Verify the Students</p>
 
+                <div className="p-4">
+                    {array.map((item, index) => (
+                        <div key={index} className=" bg-gray-500 rounded-lg p-4 m-5 w-full shadow-md ">
+                            <p className="text-xl font-bold">Name: {item.user.firstName} {item.user.lastName}</p>
 
-
-                <p class="text-xl font-bold">Admin View</p>
-                <p class ="text-base">Verify the Students</p>
-
-                <div class="p-4">
-                    {array.map((item) => {
-                        <div class="bg-white border border-gray-300 rounded-lg p-4 shadow-md mr-20 ">
-                            <p class="text-xl font-bold">Name: {item.user.firstName} {item.user.lastName}</p>
-
-                            <p class="font-bold">Username: {item.user.username}</p>
-                            <p class="font-bold">Email: {item.user.email}</p>
+                            <p className="font-bold">Username: {item.user.username}</p>
+                            <p className="font-bold">Email: {item.user.email}</p>
+                            {index < 4 && <div className="text-red-500">
+                                Needs verification!
+                                </div>}
                         
 
-                            <div class="bg-white border border-gray-300 rounded-lg p-4 shadow-md mr-20 ">         
+                            <div className="bg-white border border-gray-300 rounded-lg p-4 shadow-md mr-20 ">         
                                     
+                                <label for={"checkboxGroup"} className="font-bold text-xl mb-4">Verify Completed Courses</label>
 
                                 <CheckboxGroup
-                                    label="Which Certification has been Completed"
+                                    id="checkboxGroup"
                                     defaultValue={["buenos-aires", "london"]}
 
                                 >
-                                    {certifications.map((cert) => (
-                                        <Checkbox value={cert.name} >{cert.category} - {cert.name}</Checkbox>
+                                    {certifications.map((cert, ind) => (
+                                        <div>
+                                            <Checkbox id={"cert" + ind} key={ind} value={cert.name} className=""></Checkbox>
+                                            <label for={"cert" + ind}>{cert.category} - {cert.name}</label>
+                                        </div>
                                     ))}
 
 
@@ -64,7 +68,7 @@ export default function App() {
 
                             </div>
                         </div>
-                    }
+                    )
 
                     )}
                     
