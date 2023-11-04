82a605c70964176444297fd19080ed9c6e1b3075
Finn Bledsoe <bledsoef@berea.edu>
2023-11-04 08:38:40 -0400

    modified the outreach board

--- control-r-frontend/app/outreachBoard/page.js
+++ control-r-frontend/app/outreachBoard/page.js
@@ -3,67 +3,41 @@ import { authentication } from "@/firebaseConfig";
 import { signInWithEmailAndPassword } from "firebase/auth"
 import { useState } from "react";
 import Link from "next/link";
-import { Input } from "@nextui-org/react";
-import { Button, ButtonGroup } from "@nextui-org/react";
+import {
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+  } from "@nextui-org/react";
 
 export default function OutreachBoard() {
 
+    const dummyArr = [["The Linden Foundation", "The Linden Foundation helps Americans through family support resources, economic programs, and housing opportunities", "https://www.lindenfoundation.org/"],
+                    ["United Methodist Church & Community Development For All People", "The Community Development For All People is a faith based partner in Healthy Neighborhoods Healthy Family which assists in developing healthy food options.", "https://www.4allpeople.org/"], 
+                    ["Community Shelter Board", "Community Shelter Board provides rapid rehousing efforts for Linden residents threatened by homelessness.", "https://www.csb.org/"]
+            ]
 
     return (
-        <div className="flex w-full h-full justify-center items-center">
-            <div className="p-3 flex-col w-1/2 justify-center">
-                <p className="flex text-xl font-bold p-3 justify-center items-center">Related Groups:</p>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>
-                                    Name of Orgranization:
-                                </th>
-                                <th>
-                                    Description:
-                                </th>
-                                <th>
-                                    More Information Here:
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    The Linden Foundation
-                                </td>
-                                <td>
-                                    The Linden Foundation helps Americans through family support resources, economic programs, and housing opportunities.
-                                </td>
-                                <td>
-                                    <a href="https://www.lindenfoundation.org/">https://www.lindenfoundation.org/</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    United Methodist Church & Community Development For All People
-                                </td>
-                                <td>
-                                    The Community Development For All People is a faith based partner in Healthy Neighborhoods Healthy Family which assists in developing healthy food options. 
-                                </td>
-                                <td>
-                                    <a href="https://www.4allpeople.org/">https://www.4allpeople.org/</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> 
-                                    Community Shelter Board
-                                </td>
-                                <td>
-                                    Community Shelter Board provides rapid rehousing efforts for Linden residents threatened by homelessness.
-                                </td>
-                                <td>
-                                    <a href="https://www.csb.org/">https://www.csb.org/</a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-            </div>
+        <div className="w-full h-full justify-center items-center p-8">
+            <p className="flex justify-center text-4xl font-bold p-5">Partner Organizations</p>
+            <Table isStriped aria-label="Example static collection table justify-center w-1/2">
+          <TableHeader>
+            <TableColumn className="text-center">Name of Organization</TableColumn>
+            <TableColumn className="text-center">Description</TableColumn>
+            <TableColumn className="text-center">More Information</TableColumn>
+          </TableHeader>
+          <TableBody>
+            {dummyArr.map(([name, description, info], ind) => (
+              <TableRow key={ind}>
+                <TableCell className="text-center">{name}</TableCell>
+                <TableCell className="text-center">{description}</TableCell>
+                <TableCell className="text-center">{info}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
         </div>
     );
 
