3aff7b1831e7a8d7a696eb6f66ea64193d8dc661
Wrn13 <alphwrn@gmail.com>
2023-11-03 22:19:21 -0400

    Finished xp request

--- Backend/team17ctrlr/src/api/incentiveControl/createIncentive.js
+++ Backend/team17ctrlr/src/api/incentiveControl/createIncentive.js
@@ -4,13 +4,15 @@ const  router = express.Router();
 
 
 router.post('/', async (req,res) => {
-    const reqxp = req.body.xp;
-    const uid = req.body.uid;
-    //const uid = 'ir2Dd0EAsqc07zfAWQAQ';
+    const reqxp = parseInt(req.body.xp);
+
+    console.log("REQUEST XP: " + reqxp);
+    //const uid = req.body.uid;
+    const uid = 'ir2Dd0EAsqc07zfAWQAQ';
     var result = await admin.app().firestore().collection('Points').doc(uid).get();
     //console.log(result);
 
-    var xp = result.data().xp + reqxp;
+    var xp = parseInt(result.data().xp) + reqxp;
     var level = result.data().level;
     var totalPoints = result.data().totalPoints;
 
@@ -20,6 +22,10 @@ router.post('/', async (req,res) => {
 
         xp = xp % 2000;
     }
+
+    console.log("NEW XP: " + xp);
+
+
 
 
     admin.firestore().collection('Points').doc(uid).update({'xp':xp, 'level':level, 'totalPoints':totalPoints}).then((snapshot) =>
