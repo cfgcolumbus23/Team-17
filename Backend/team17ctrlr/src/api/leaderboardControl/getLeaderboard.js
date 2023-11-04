e6fdf71e8973ab995a0dfdab7c3fb5c585bed82c
Wrn13 <alphwrn@gmail.com>
2023-11-04 00:47:57 -0400

    Finished leaderboard endpoint

--- Backend/team17ctrlr/src/api/leaderboardControl/getLeaderboard.js
+++ Backend/team17ctrlr/src/api/leaderboardControl/getLeaderboard.js
@@ -1,17 +1,17 @@
 const express = require('express');
-
-
-const express = require('express');
 const router = express.Router();
 var admin = require("firebase-admin");
 
 router.get('/', (req, res) => {
+    //Get results ordered by level descending
     const collectionRef = admin.app().firestore().collection('Points').orderBy('level', 'desc');
     collectionRef.get().then((snapshot)=>{
         const results = [];
+        //Add results to the list
         snapshot.forEach(element => {
             results.push(element.data())
         });
+        //Return list
         console.log(results);
         res.send(results);
     }).catch((error)=>{
@@ -20,6 +20,4 @@ router.get('/', (req, res) => {
     });
 });
 
-
-
-
+module.exports = router
