3921b338b3c6044927f886a2fd410c48b42d2811
MY NAME <86694889+Mehult03@users.noreply.github.com>
2023-11-04 06:26:33 -0400

    Testimonials PR

--- control-r-frontend/app/page.js
+++ control-r-frontend/app/page.js
@@ -72,21 +72,21 @@ Our comprehensive programs can open doors to entry-level IT positions in a rapid
         <div class="flex flex-wrap">
           <div class="w-full md:w-1/3 px-2 mb-4">
             <div class="bg-white rounded shadow py-2">
-              <p class="text-gray-800 text-base px-6 mb-5">Monitoring and tracking my health vitals anywhere I go and on any platform I use has never been easier.</p>
-              <p class="text-gray-500 text-xs md:text-sm px-6">John Doe</p>
+              <p class="text-gray-800 text-base px-6 mb-5">CTRL-R has helped my kids learn many skills. Seeing them motivated to learn makes me so happy!</p>
+              <p class="text-gray-500 text-xs md:text-sm px-6">Jane Doe</p>
             </div>
             
           </div>
           <div class="w-full md:w-1/3 px-2 mb-4">
             <div class="bg-white rounded shadow py-2">
-              <p class="text-gray-800 text-base px-6 mb-5">As an Athlete, this is the perfect product for me. I wear my Smart Health Monitoring Wristwatch everywhere I go, even in the bathroom since it's waterproof.</p>
-              <p class="text-gray-500 text-xs md:text-sm px-6">Jane Doe</p>
+              <p class="text-gray-800 text-base px-6 mb-5">I've been using CTRL-R to help me get my A+ Certifications and I get paid while doing it!</p>
+              <p class="text-gray-500 text-xs md:text-sm px-6">Warren Smith</p>
             </div>
           </div>
           <div class="w-full md:w-1/3 px-2 mb-4">
             <div class="bg-white rounded shadow py-2">
-              <p class="text-gray-800 text-base px-6 mb-5">I don't regret buying this wearble gadget. One of the best gadgets I own!.</p>
-              <p class="text-gray-500 text-xs md:text-sm px-6">James Doe</p>
+              <p class="text-gray-800 text-base px-6 mb-5">As an employee at Pyramid CDC, CTRL-R has truly made a difference for our students.</p>
+              <p class="text-gray-500 text-xs md:text-sm px-6">Mark Finn</p>
             </div>
           </div>
         </div>
